cb631e224b0f923e5bb991079a2289f3ac9f7e49
Zarahy <chaparrozarahy@gmail.com>
2024-01-02 21:05:52 -0500

    Proyecto WebService Api

---
+++ WebServiceApi/Controller/SumaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebServiceApi.Modelo;
+
+namespace WebServiceApi.Controller
+{
+    [Route("api/suma")]
+    [ApiController]
+    public class SumaController
+    {
+        [HttpGet]
+        [Route("sumar")]
+        public ActionResult<ProcedimientoSuma> Sumar(int numero1, int numero2)
+        {
+            ProcedimientoSuma calculadora = new ProcedimientoSuma(numero1, numero2);
+            calculadora.suma();
+
+            return calculadora;
+        }
+    }
+}
---
+++ WebServiceApi/Modelo/ProcedimientoSuma.cs
@@ -0,0 +1,20 @@
+﻿namespace WebServiceApi.Modelo
+{
+    public class ProcedimientoSuma
+    {
+        public int primerdigito { get; set; }
+        public int segundodigito { get; set; }
+        public int total { get; set; }
+
+        public ProcedimientoSuma(int primerdigito, int segundodigito)
+        {
+            this.primerdigito = primerdigito;
+            this.segundodigito = segundodigito;
+        }
+
+        public void suma()
+        {
+            total = primerdigito + segundodigito;
+        }
+    }
+}
